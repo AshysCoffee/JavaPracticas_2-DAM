f38de318e6b6a325f0755caf3c7a4209d2d5a5f9
Ashy's Coffee <ashleyleonidk@gmail.com>
2025-10-30 23:07:23 +0100

    Ejercicio con SQL

--- pruebas_bbdd/src/pruebas_bbdd/EjercicioNBA.java
+++ pruebas_bbdd/src/pruebas_bbdd/EjercicioNBA.java
@@ -1,23 +1,12 @@
 package pruebas_bbdd;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-/*1. Funcion que permite obtener los datos de los jugadores que empiecen por una letra -> Select * from jugadores where nombre like '?'
- *parametro -> 'A'+'%'
- * 2. EL peso medio de los jugadores AVG :^P
- * 3. Listando los equipos que nos permita seleccionar el número del equipo y podamos ver todos los integrantes del equipo seleccionado
- * 4. Inserta un jugador xd
- * 
- * el execute boolean da un false o un resultset*/
-
-//tengo que hacer un programa osea wtf xD
-
 
 public class EjercicioNBA {
 
@@ -25,7 +14,7 @@ public class EjercicioNBA {
 		
 		String url = "jdbc:mysql://localhost:3306/nba";
 		String usuario = "root";
-		String password = "cfgs";
+		String password = "Soyunestegosaurio27.";
 		
 		
 		Scanner sc = new Scanner (System.in);
@@ -52,7 +41,7 @@ public class EjercicioNBA {
 					
 					Connection conexion = DriverManager.getConnection(url,usuario,password);
 
-					String condicion = "'"+letra+"%'";
+					String condicion = letra+"%";
 					
 					String consulta = "SELECT * FROM jugadores WHERE Nombre LIKE ? ";
 					PreparedStatement sentencia_preparada = conexion.prepareStatement(consulta);	
@@ -60,19 +49,25 @@ public class EjercicioNBA {
 
 					ResultSet resultado = sentencia_preparada.executeQuery();		
 					
+					System.out.println("Jugadores que empiezan por "+letra+" :-------");
+					
 					while (resultado.next()){
 						int codigo = resultado.getInt("codigo");
-						String nombre = resultado.getNString("Nombre");
-						String procedencia = resultado.getNString("Procedencia");
+						String nombre = resultado.getString("Nombre");
+						String procedencia = resultado.getString("Procedencia");
+						String altura = resultado.getString("Altura");
 						int peso = resultado.getInt("Peso");
-						String posicion = resultado.getNString("Posicion");
-						String nombre_equipo = resultado.getNString("Nombre_equipo");
+						String posicion = resultado.getString("Posicion");
+						String nombre_equipo = resultado.getString("Nombre_equipo");
 						
-						System.out.println("Codigo:"+codigo+" | Nombre: "+nombre+" | Procedencia: "+procedencia+
-								"| Peso: "+peso+" | Posición:"+posicion+" | Nombre del equipo:"+nombre_equipo+"|");
+						
+						System.out.println("---\nCodigo:"+codigo+"\nNombre: "+nombre+"\nProcedencia: "+procedencia+
+								"\nAltura: "+altura+"\nPeso: "+peso+"\nPosición:"+posicion+"\nNombre del equipo:"+nombre_equipo+"\n-----\n");
 						
 				
 					}	
+					
+					System.out.println("--------------");
 					
 				}catch(Exception e) {
 					e.printStackTrace();
@@ -95,7 +90,7 @@ public class EjercicioNBA {
 					while (resultado.next()){
 						int peso_medio = resultado.getInt("peso_promedio");
 			
-						System.out.println("Peso medio de toda la selección: "+peso_medio+" kg. \n");
+						System.out.println("Peso medio de toda la selección: "+peso_medio+" lbs. \n");
 						
 					}	
 					
@@ -103,17 +98,162 @@ public class EjercicioNBA {
 					e.printStackTrace();
 				}
 				
+				break;
+				
 			case 3:
 				
+				ArrayList <String> opciones = new ArrayList<>();
+				
+				try {
+					
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection conexion = DriverManager.getConnection(url,usuario,password);
+			
+					Statement sentencia = conexion.createStatement();	
+					String consulta = "SELECT DISTINCT nombre_equipo FROM jugadores";
+					ResultSet resultado = sentencia.executeQuery(consulta);		
+					
+					int contador = 0;
+					
+					
+					System.out.println("\nEquipos existentes:---------------");
+					
+					while (resultado.next()){
+						String nombre_equipo = resultado.getString("nombre_equipo");
+						contador++;
+						System.out.println(contador +". "+ nombre_equipo+"");
+						
+						opciones.add(nombre_equipo);
+						
+					}	
+					
+					System.out.println("\n");
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				
+				
+				int seleccion=0;
+				
+				System.out.print("Escriba el número del equipo que desea consultar: ");
+				seleccion=sc.nextInt();
+				
+				
+				try {
+					
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					
+					Connection conexion = DriverManager.getConnection(url,usuario,password);
+					String consulta = "SELECT * FROM jugadores WHERE nombre_equipo = ?";
+					PreparedStatement sentencia_preparada = conexion.prepareStatement(consulta);	
+					
+					String condicion = opciones.get(seleccion-1);
+					
+					sentencia_preparada.setString(1, condicion);
+					
+					
+					ResultSet resultado = sentencia_preparada.executeQuery();
+					
+					System.out.println("Equipo "+opciones.get(seleccion-1).toString()+":");
+					
+					while (resultado.next()){
+						int codigo = resultado.getInt("codigo");
+						String nombre = resultado.getString("Nombre");
+						String procedencia = resultado.getString("Procedencia");
+						int peso = resultado.getInt("Peso");
+						String posicion = resultado.getString("Posicion");
+						
+						
+						System.out.println("---\nCodigo:"+codigo+"\nNombre: "+nombre+"\nProcedencia: "+procedencia+
+								"\nPeso: "+peso+"\nPosición:"+posicion+"\n-----\n");
+						
+				
+					}	
+					
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+
+				break;
+
 			case 4:
+
+				System.out.println("Por favor, introduzca los siguientes datos:");
 				
+				System.out.println("Codigo: ");
+				int codigo = sc.nextInt();
 				
+				sc.nextLine();
 				
-			}
+				System.out.println("Nombre: ");
+				String nombre = sc.nextLine();
+
+				System.out.println("Procedencia: ");
+				String procedencia = sc.nextLine();
+
+				System.out.println("Altura: ");
+				String altura = sc.nextLine();
+				
+				System.out.println("Peso: ");
+				int peso = sc.nextInt();
+
+				sc.nextLine();
+				
+				System.out.println("Posicion: ");
+				String posicion = sc.nextLine();
+
+				System.out.println("Nombre de su equipo: ");
+				String nombre_equipo = sc.nextLine();
+		
+				
+
+				try {
+
+					Class.forName("com.mysql.cj.jdbc.Driver");
+
+					Connection conexion = DriverManager.getConnection(url,usuario,password);
+					String consulta = "INSERT INTO jugadores VALUES (?,?,?,?,?,?,?)";
+					PreparedStatement sentencia_preparada = conexion.prepareStatement(consulta);	
+
+					sentencia_preparada.setInt(1, codigo);
+					sentencia_preparada.setString(2, nombre);
+					sentencia_preparada.setString(3, procedencia);
+					sentencia_preparada.setString(4, altura);
+					sentencia_preparada.setInt(5, peso);
+					sentencia_preparada.setString(6, posicion);
+					sentencia_preparada.setString(7, nombre_equipo);
+					
+					int resultado = sentencia_preparada.executeUpdate();
+								
+					if (resultado > 0) {
+				        
+						System.out.println("---\nCodigo:"+codigo+"\nNombre: "+nombre+"\nProcedencia: "+procedencia+
+								"\nAltura: "+altura+"\nPeso: "+peso+"\nPosición:"+posicion+"\nNombre del equipo:"+nombre_equipo+"\n-----\n");
+						
+				    } else {
+				    	
+				        System.out.println("No se pudo insertar el jugador");
+				    }
+					
+
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+
+				
+			default:
+				System.out.println("No es una opcion");
+			
+					
+					
+			}		
 			
 			
 		}while (opcion<5);
 
+		sc.close();
+		
 	}
 
 }
