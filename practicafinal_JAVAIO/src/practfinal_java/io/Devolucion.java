be13a58767633604f763ec052ec848d9763ec5c4
Ashy's Coffee <ashleyleonidk@gmail.com>
2025-11-11 17:49:22 +0100

    Desarrollo clase Devolucion

--- practicafinal_JAVAIO/src/practfinal_java/io/Devolucion.java
+++ practicafinal_JAVAIO/src/practfinal_java/io/Devolucion.java
@@ -1,5 +1,36 @@
 package practfinal_java.io;
 
 public class Devolucion {
+     private Venta venta;
+     private Planta planta
+
+     public Devolucion (Venta venta, Planta planta){
+
+         this.venta = venta;
+         this.planta = planta;
+
+     }
+
+    public void setVenta (Venta venta){
+        this.venta=venta;
+    }
+    
+    public Venta getVenta(){
+        return this.venta;
+    }
+
+
+
+    public void setPlanta(Planta planta){
+        this.planta = planta;
+    }
+
+    public Planta getPlanta(){
+         return Planta
+    }
+
+
+    
+
 
 }
